75c4ca91f3f692d0d16c1222926e6c04db7b1180
automation curry puff <36359269+autocurry@users.noreply.github.com>
2019-02-11 15:10:43 +0800

    Update CreateHtml.cs

--- Files/CreateHtml.cs
+++ Files/CreateHtml.cs
@@ -4,54 +4,57 @@ using System.IO;
 using Newtonsoft.Json;
 public class CreateHtml{
 
-
-        public bool CreateHTMLMonitor(string input, string output)
+ public string CreateHTMLMonitor(List<ResponseClass> _responseList, string output)
         {
+
+            //string json = File.ReadAllText(input);
+            //var _response = JsonConvert.DeserializeObject<List<ResponseClass>>(json);
             
-            string json = File.ReadAllText(input);
-            var _response = JsonConvert.DeserializeObject<List<ResponseClass>>(json);
+            string html = File.ReadAllText(DirectoryHelper.BaseDirectory+ "\\APIMonitor\\APIMonitorPage.html");
+            string _ReplaceHeaderValues = ReplacingHeaderValues(html);
+            string _ReplaceContentValues = ReplaceContentValues(_ReplaceHeaderValues, _responseList);
+            string outputfile = DirectoryHelper.BaseDirectory + "\\APIMonitor\\" + output;
 
-            string html = File.ReadAllText(".//Files//sample.html");
-            string ReplaceHeaderValues = ReplacingHeaderValues(html);
-           // string ReplaceContentValues = ReplaceContentValues(ReplaceHeaderValues,_response);
+            File.WriteAllText(outputfile, _ReplaceContentValues);
 
+            return outputfile;
 
-           File.WriteAllText(".//Files//"+output,ReplaceHeaderValues);
-
-            return true;
-            
         }
 
         public string ReplacingHeaderValues(string html)
         {
-             string Env = "SIT";
-             string Executiontime = DateTime.Now.ToString();
-                string ExecutionUser = "chandrs";
+            string Env = "EnvName";
+            string Executiontime = DateTime.Now.ToString();
+            string ExecutionUser = "userName";
 
-                string newstring1 = html.Replace("EnvPlaceHolder",Env);
-                string newstring2 = newstring1.Replace("ExecutionTimePlaceHolder",Executiontime);
-                string newstring3 = newstring2.Replace("ExecutionUserPlaceHolder",ExecutionUser);
-                
-                return newstring3;
+            string newstring1 = html.Replace("EnvPlaceHolder", Env);
+            string newstring2 = newstring1.Replace("ExecutionTimePlaceHolder", Executiontime);
+            string newstring3 = newstring2.Replace("ExecutionUserPlaceHolder", ExecutionUser);
+
+            return newstring3;
 
         }
 
-    // public string ReplaceContentValues(string html,List<ResponseClass> _list)
-    //     {
+
+        public string ReplaceContentValues(string html,List<ResponseClass> responseList)
+        {
+            string value = string.Empty;
             
-
-    //             string newstring1 = html.Replace("EnvPlaceHolder",Env);
-    //             string newstring2 = newstring1.Replace("ExecutionTimePlaceHolder",Executiontime);
-    //             string newstring3 = newstring2.Replace("ExecutionUserPlaceHolder",ExecutionUser);
-                
-    //             return newstring3;
-
-    //     }
+            string replacerows = string.Empty;
 
 
+            foreach(ResponseClass item in responseList)
+            {
+                replacerows = replacerows + "<tr><td>" + item.APIURL + "</td><td>" + item.Status + "</td><td>" + item.StatusCode + "</td><td>" + item.ResponseTime + "</td><td>ResponseContent</td></tr>";
 
+            }
+       
 
+            string newstring = html.Replace("MonitorPlaceHolder", replacerows);          
 
+            return newstring;
 
+        }
+      
 
 }
