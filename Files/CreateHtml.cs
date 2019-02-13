2f6070077770aa7fd336884a509ec47250db2920
sandeep <sandeepsekhar555@gmail.com>
2019-02-13 23:19:53 +0800

    onload color change enabled

--- Files/CreateHtml.cs
+++ Files/CreateHtml.cs
@@ -10,54 +10,55 @@ public class CreateHtml{
             //string json = File.ReadAllText(input);
             //var _response = JsonConvert.DeserializeObject<List<ResponseClass>>(json);
             
-            string html = File.ReadAllText(DirectoryHelper.BaseDirectory+ "\\APIMonitor\\APIMonitorPage.html");
-            string _ReplaceHeaderValues = ReplacingHeaderValues(html);
-            string _ReplaceContentValues = ReplaceContentValues(_ReplaceHeaderValues, _responseList);
-            string outputfile = DirectoryHelper.BaseDirectory + "\\APIMonitor\\" + output;
+        //     string html = File.ReadAllText(DirectoryHelper.BaseDirectory+ "\\APIMonitor\\APIMonitorPage.html");
+        //     string _ReplaceHeaderValues = ReplacingHeaderValues(html);
+        //     string _ReplaceContentValues = ReplaceContentValues(_ReplaceHeaderValues, _responseList);
+        //     string outputfile = DirectoryHelper.BaseDirectory + "\\APIMonitor\\" + output;
 
-            File.WriteAllText(outputfile, _ReplaceContentValues);
+        //     File.WriteAllText(outputfile, _ReplaceContentValues);
 
-            return outputfile;
+        //     return outputfile;
 
-        }
+        // }
 
-        public string ReplacingHeaderValues(string html)
-        {
-            string Env = "EnvName";
-            string Executiontime = DateTime.Now.ToString();
-            string ExecutionUser = "userName";
+        // public string ReplacingHeaderValues(string html)
+        // {
+        //     string Env = "EnvName";
+        //     string Executiontime = DateTime.Now.ToString();
+        //     string ExecutionUser = "userName";
 
-            string newstring1 = html.Replace("EnvPlaceHolder", Env);
-            string newstring2 = newstring1.Replace("ExecutionTimePlaceHolder", Executiontime);
-            string newstring3 = newstring2.Replace("ExecutionUserPlaceHolder", ExecutionUser);
+        //     string newstring1 = html.Replace("EnvPlaceHolder", Env);
+        //     string newstring2 = newstring1.Replace("ExecutionTimePlaceHolder", Executiontime);
+        //     string newstring3 = newstring2.Replace("ExecutionUserPlaceHolder", ExecutionUser);
 
-            return newstring3;
+        //     return newstring3;
 
-        }
+        // }
 
 
-         public string ReplaceContentValues(string html,List<ResponseClass> responseList)
-        {
-            string value = string.Empty;
-            int rownum = 1;
+        //  public string ReplaceContentValues(string html,List<ResponseClass> responseList)
+        // {
+        //     string value = string.Empty;
+        //     int rownum = 1;
             
-            string replacerows = string.Empty;
-            string DetailsDiv = string.Empty;
+        //     string replacerows = string.Empty;
+        //     string DetailsDiv = string.Empty;
 
 
-            foreach(ResponseClass item in responseList)
-            {
-                replacerows = replacerows + "<tr><td>" + item.APIURL + "</td><td>" + item.Status + "</td><td>" + item.StatusCode + "</td><td>" + item.ResponseTime + "</td><td><a id="+ rownum + " href=\"#\" onclick=\"toggleAppearance(this.id);\">ResponseContent</a></td></tr>";
-                DetailsDiv = DetailsDiv + "<div id=details" + rownum + " class=detailsdiv>" + item.Response+"</div>";
-                rownum= rownum + 1;
-            }
+        //     foreach(ResponseClass item in responseList)
+        //     {
+        //         replacerows = replacerows + "<tr><td>" + item.APIURL + "</td><td>" + item.Status + "</td><td>" + item.StatusCode + "</td><td>" + item.ResponseTime + "</td><td><a id="+ rownum + " href=\"#\" onclick=\"toggleAppearance(this.id);\">ResponseContent</a></td></tr>";
+        //         DetailsDiv = DetailsDiv + "<div id=details" + rownum + " class=detailsdiv>" + item.Response+"</div>";
+        //         rownum= rownum + 1;
+        //     }
        
 
-            string newstring = html.Replace("MonitorPlaceHolder", replacerows);
-            newstring = newstring.Replace("PlaceHolderForDetailsDIV", DetailsDiv);
+        //     string newstring = html.Replace("MonitorPlaceHolder", replacerows);
+        //     newstring = newstring.Replace("PlaceHolderForDetailsDIV", DetailsDiv);
 
-            return newstring;
+        //     return newstring;
+        return "";
 
-        }
+         }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
 
 namespace APIMonitor
 {
@@ -7,11 +8,33 @@ namespace APIMonitor
         string inputfile = string.Empty;
         string outputfile = string.Empty;
         
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
+
+            //var status =new CreateHtml().CreateHTMLMonitor(".//Files//reponsemonitor.json","ApiMonitor.html");
+            User user = new User();
+            user.id = "anju";
+            user.email ="sandeep";
+            string url = "https://{user.id}/{user.email}/2";
+            string url1 = "https://{name}/2";
+
+                var result = await CSharpScript.EvaluateAsync<string>(
+    "var name = \"" + user + "\"; " +
+    "return $\"" + url + "\";");
+
             
-            var status =new CreateHtml().CreateHTMLMonitor(".//Files//reponsemonitor.json","ApiMonitor.html");
+            Console.WriteLine(result);
+
         }
 
+       
+
     }
+
+    public class User{
+             public string id = "1234";
+             public string email = "sandeepsekhar555";
+    }
+
+   
 }
